e2df8305a4b7c1b2321c8d4095dd70a83fe101d8
NIHILncunia <nihil_ncunia@naver.com>
2026-06-28 04:26:37 +0900

    2026 0628 feat: Quartz 웹 뷰어 초기 설정 및 GitHub Pages 배포 파이프라인 구성
    - quartz 폴더에 Quartz v4 설치
    
    - docs.nihilncunia.com 도메인 설정
    
    - 비밀 설정, 설정 정비, 세력 관계, 스토리 설계 폴더 빌드 제외
    
    - 이미지 경로 오류 수정을 위해 markdownLinkResolution을 relative로 변경
    
    - GitHub Pages 자동 배포 워크플로우 추가

---
+++ quartz/quartz.config.ts
@@ -0,0 +1,105 @@
+﻿import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "프로젝트 위그드라실",
+    pageTitleSuffix: "",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: null,
+    },
+    locale: "ko-KR",
+    baseUrl: "docs.nihilncunia.com",
+    ignorePatterns: [
+      "위그드라실/비밀 설정/**",
+      "위그드라실/설정 정비/**",
+      "위그드라실/세력 관계/**",
+      "위그드라실/스토리 설계/**",
+      "룩스테라/비밀 설정/**",
+      "룩스테라/설정 정비/**",
+      "룩스테라/세력 관계/**",
+      "룩스테라/스토리 설계/**",
+      "엘드로스/비밀 설정/**",
+      "엘드로스/설정 정비/**",
+      "엘드로스/세력 관계/**",
+      "엘드로스/스토리 설계/**",
+      ".obsidian",
+    ],
+    defaultDateType: "modified",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#faf8f8",
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#284b63",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#fff23688",
+        },
+        darkMode: {
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#7b97aa",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#b3aa0288",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "git", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "relative" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.Favicon(),
+      Plugin.NotFoundPage(),
+      Plugin.CustomOgImages(),
+    ],
+  },
+}
+
+export default config
